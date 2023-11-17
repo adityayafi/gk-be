f00619624fb63f48a64f26f23e45ff06f33cf6a9
aditya yafi <adityayafi@gmail.com>
2023-11-17 12:52:59 +0700

    minor bug fix

--- migrations/20231116042009-create-product.js
+++ migrations/20231116042009-create-product.js
@@ -2,7 +2,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('products', {
+    await queryInterface.createTable('product', {
       id: {
         type: Sequelize.UUID,
         allowNull: false,
@@ -45,6 +45,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('products');
+    await queryInterface.dropTable('product');
   }
 };
