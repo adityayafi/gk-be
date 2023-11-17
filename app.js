b913b7d95aa292ed6408fb72768d8bd97bc597c4
aditya yafi <adityayafi@gmail.com>
2023-11-17 13:47:31 +0700

    add root api

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use('/', indexRouter);
+app.use('/', indexRouter);
 app.use('/api', usersRouter);
 app.use('/api', productRouter);
 // app.use('/users', usersRouter);
